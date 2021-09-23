45b1a953539a7cbaefff432167cac281bce0ed24
dfder <a2470abc@gmail.com>
2021-09-24 01:00:32 +0800

    check bk path

--- Scripts/rsync_to_lab_nas.sh
+++ Scripts/rsync_to_lab_nas.sh
@@ -2,4 +2,4 @@
 #sshpass -p "密碼" but not work
 time rsync -avh --delete --stats --info=progress2 -e "ssh -p 2222" --exclu
 de H@H --exclude .conda --exclude .cache --exclude .vscode-server ./ df@14
-0.121.196.20:~/home_bk | lolcat
+0.121.196.20:~/bk/homeDir/df-c | lolcat
