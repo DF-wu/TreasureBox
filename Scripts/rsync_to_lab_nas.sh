c82feca7aeceb76448009b9535ea9f6c698b71b8
dfder <a2470abc@gmail.com>
2021-09-24 01:01:38 +0800

    remove \n

--- Scripts/rsync_to_lab_nas.sh
+++ Scripts/rsync_to_lab_nas.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #sshpass -p "密碼" but not work
-time rsync -avh --delete --stats --info=progress2 -e "ssh -p 2222" --exclu
-de H@H --exclude .conda --exclude .cache --exclude .vscode-server ./ df@14
-0.121.196.20:~/bk/homeDir/df-c | lolcat
+time rsync -avh --delete --stats --info=progress2 -e "ssh -p 2222" --exclude H@H --exclude .conda --exclude .cache --exclude .vscode-server ./ df@140.121.196.20:~/bk/homeDir/df-c | lolcat
