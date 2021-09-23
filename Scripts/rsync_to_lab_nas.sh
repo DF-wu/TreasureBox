8229281e96597a85cc5703cb99b779f5bc6e0c73
dfder <a2470abc@gmail.com>
2021-09-24 01:21:05 +0800

    change name

--- Scripts/rsync_to_lab_nas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#sshpass -p "密碼" but not work
-time rsync -avh --delete --stats --info=progress2 -e "ssh -p 2222" --exclude H@H --exclude .conda --exclude .cache --exclude .vscode-server ./ df@140.121.196.20:~/bk/homeDir/df-c | lolcat
