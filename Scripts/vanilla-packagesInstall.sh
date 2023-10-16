4f34f60aa67a7234e6cab7e190985e4e5f2b0fe2
dfder <a2470abc@gmail.com>
2023-10-16 16:00:52 +0800

    update package

--- Scripts/vanilla-packagesInstall.sh
+++ Scripts/vanilla-packagesInstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # for debian
-sudo apt install duf bat htop btop byobu lolcat neofetch jq curl ethtool zsh docker docker-compose lsd tree rsync rclone timeshift strace neovim -y
+sudo apt install duf bat htop btop byobu lolcat neofetch jq curl ethtool zsh docker docker-compose lsd tree rsync rclone timeshift strace neovim open-vm-tools -y
