0d8cf0bd38668c8d97f6833b2b1b1a2f43537d38
dfder <a2470abc@gmail.com>
2023-10-16 15:55:29 +0800

    fix typo

--- Scripts/vanilla-packagesInstall.sh
+++ Scripts/vanilla-packagesInstall.sh
@@ -1,1 +1,3 @@
-sudo apt install duf batcat htop btop byobu lolcat neofetch jq curl ethtool zsh docker docker-compose lsd tree rsync rclone timeshift strace neovime -y
+#!/bin/bash
+# for debian
+sudo apt install duf bat htop btop byobu lolcat neofetch jq curl ethtool zsh docker docker-compose lsd tree rsync rclone timeshift strace neovim -y
