3e0208fbfd02e3b155208adaaf8fd8a9854aeac7
dfder <a2470abc@gmail.com>
2023-10-20 18:09:19 +0800

    add reference url. modify path

--- cronjob/shopee-coin-claim.sh
+++ cronjob/shopee-coin-claim.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Author: df
+# reference : https://github.com/wdzeng/shopee-coins-bot
 docker pull ghcr.io/wdzeng/shopee-coins-bot
-time docker run --rm -v /mnt/user/appdata/shopee-sign-bot/cookie:/cookie  ghcr.io/wdzeng/shopee-coins-bot  -c /cookie
+time docker run --rm -v mnt/appdata/shopee-sign-bot/cookie:/cookie  ghcr.io/wdzeng/shopee-coins-bot  -c /cookie
