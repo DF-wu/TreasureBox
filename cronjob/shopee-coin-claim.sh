df3f06a341501de9a4ff140f18d1de78cd03d091
dfder <a2470abc@gmail.com>
2023-10-20 18:11:25 +0800

    fix typo.

--- cronjob/shopee-coin-claim.sh
+++ cronjob/shopee-coin-claim.sh
@@ -2,4 +2,4 @@
 # Author: df
 # reference : https://github.com/wdzeng/shopee-coins-bot
 docker pull ghcr.io/wdzeng/shopee-coins-bot
-time docker run --rm -v mnt/appdata/shopee-sign-bot/cookie:/cookie  ghcr.io/wdzeng/shopee-coins-bot  -c /cookie
+time docker run --rm -v /mnt/appdata/shopee-sign-bot/cookie:/cookie  ghcr.io/wdzeng/shopee-coins-bot  -c /cookie
