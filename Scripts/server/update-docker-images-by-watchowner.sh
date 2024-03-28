7e29aa2e1c0aa2c928b0b786d35d8b29ea6f0154
D.F. <df@dfder.tw>
2024-03-28 18:23:13 +0800

    feat: update container with a elegant way.

---
+++ Scripts/server/update-docker-images-by-watchowner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Author: df
+# Date: 2024-03-28
+# Description: Update all docker images on the server by watchowner
+
+
+docker run --rm   --name watchtower     --volume /var/run/docker.sock:/var/run/docker.sock     containrrr/watchtower --run-once 
