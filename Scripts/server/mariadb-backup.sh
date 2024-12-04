5f26c72fa22da45b8c2e021eebefc4e7e84e5244
D.F. <df@dfder.tw>
2024-12-04 17:26:22 +0800

    fix: mysql command flag no margin......

--- Scripts/server/mariadb-backup.sh
+++ Scripts/server/mariadb-backup.sh
@@ -1,5 +1,6 @@
 # docker version
 # used for crontab
 # !!! 記得針對系統環境修改!!!
-
-docker exec mariadb sh -c "mysqldump -uroot -p 密碼密碼密碼 --all-databases" | gzip -c > ~/mariadb-backup-`date +%Y-\%m-\%d`.gz
+# doc: https://mariadb.com/kb/en/container-backup-and-restoration/
+# 幹他的flag要黏再一起好噁心
+docker exec mariadb sh -c "mysqldump -uroot -p密碼密碼密碼 --all-databases" | gzip -c > ~/mariadb-backup-`date +%Y-\%m-\%d`.gz
