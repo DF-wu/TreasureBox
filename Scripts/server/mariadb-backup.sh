dddd02d64dd1580fff2d939a80f31e0996e8c201
D.F. <df@dfder.tw>
2024-12-04 17:32:26 +0800

    fix: add to subfolder

--- Scripts/server/mariadb-backup.sh
+++ Scripts/server/mariadb-backup.sh
@@ -3,4 +3,4 @@
 # !!! 記得針對系統環境修改!!!
 # doc: https://mariadb.com/kb/en/container-backup-and-restoration/
 # 幹他的flag要黏再一起好噁心
-docker exec mariadb sh -c "mysqldump -uroot -p密碼密碼密碼 --all-databases" | gzip -c > ~/mariadb-backup-`date +%Y-\%m-\%d`.gz
+docker exec mariadb sh -c "mysqldump -uroot -p密碼密碼密碼 --all-databases" | gzip -c > ~/mariadb-backup/mariadb-backup-`date +%Y-\%m-\%d`.gz
