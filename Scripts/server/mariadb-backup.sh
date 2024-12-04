08a1b433606e7bd92941369ab6a69e0500cae091
D.F. <df@dfder.tw>
2024-12-04 17:14:10 +0800

    fix:  add messgage to remind myself

--- Scripts/server/mariadb-backup.sh
+++ Scripts/server/mariadb-backup.sh
@@ -2,4 +2,4 @@
 # used for crontab
 # !!! 記得針對系統環境修改!!!
 
-docker exec mariadb sh -c "mysqldump -uroot -p --all-databases" | gzip -c > ~/tmp/`date +%Y-\%m-\%d`.gz
+docker exec mariadb sh -c "mysqldump -uroot -p 密碼密碼密碼 --all-databases" | gzip -c > ~/mariadb-backup-`date +%Y-\%m-\%d`.gz
