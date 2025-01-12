1039ba12fb9a30dd9c3c7ce58326b64df732de70
D.F. <df@dfder.tw>
2025-01-12 17:09:08 +0800

    feat: add arch install script

---
+++ Scripts/server/Arch-vanilla-packagesInstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# for Arch
+sudo pacman -S duf bat htop btop byobu lolcat fastfetch jq curl ethtool zsh docker docker-compose lsd tree rsync rclone timeshift strace neovim open-vm-tools base-devel 
