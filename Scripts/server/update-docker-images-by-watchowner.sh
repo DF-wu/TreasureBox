ad280d7e56e2db4e4fb4ce2a8013acf0b5f25a05
D.F. <df@dfder.tw>
2025-04-04 09:01:24 +0800

    fix image prefix

--- Scripts/server/update-docker-images-by-watchowner.sh
+++ Scripts/server/update-docker-images-by-watchowner.sh
@@ -4,5 +4,5 @@
 # Date: 2024-03-28
 # Description: Update all docker images on the server by watchowner
 
-docker pull
+docker pull containrrr/watchtower
 docker run --rm   --name watchtower     --volume /var/run/docker.sock:/var/run/docker.sock     containrrr/watchtower --run-once 
